3708cba09865283f2bb50a30583db8327c911ca0
ThienNguyen <ngocthiennguyen2102@gmail.com>
2024-05-25 21:36:23 +0200

    Add unit test for service

---
+++ sites-service/src/test/java/dauphine/miageif/site/service/SiteServiceTest.java
@@ -0,0 +1,117 @@
+package dauphine.miageif.site.service;
+
+import dauphine.miageif.site.entity.Site;
+import dauphine.miageif.site.entity.Sport;
+import dauphine.miageif.site.repository.SiteRepository;
+import dauphine.miageif.site.repository.SportRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class SiteServiceTest {
+
+    @Mock
+    private SiteRepository siteRepository;
+
+    @Mock
+    private SportRepository sportRepository;
+
+    @InjectMocks
+    private SiteService siteService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAllSites() {
+        Site site1 = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+        Site site2 = new Site(2L, "Aquatics Center", "Paris", 2.3414, 48.8357, null);
+
+        when(siteRepository.findAll()).thenReturn(Arrays.asList(site1, site2));
+
+        List<Site> sites = siteService.getAllSites();
+        assertEquals(2, sites.size());
+        assertEquals("Stade de France", sites.get(0).getName());
+        assertEquals("Aquatics Center", sites.get(1).getName());
+    }
+
+    @Test
+    public void testGetSiteById() {
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+
+        when(siteRepository.findById(1L)).thenReturn(Optional.of(site));
+
+        Optional<Site> foundSite = siteService.getSiteById(1L);
+        assertTrue(foundSite.isPresent());
+        assertEquals("Stade de France", foundSite.get().getName());
+    }
+
+    @Test
+    public void testUpdateSite() {
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+        Site updatedDetails = new Site(null, "Stade de France Updated", "Paris Updated", 0, 0, null);
+
+        when(siteRepository.findById(1L)).thenReturn(Optional.of(site));
+        when(siteRepository.save(any(Site.class))).thenReturn(site);
+
+        Site updatedSite = siteService.updateSite(1L, updatedDetails);
+
+        assertEquals("Stade de France Updated", updatedSite.getName());
+        assertEquals("Paris Updated", updatedSite.getLocation());
+    }
+
+    @Test
+    public void testDeleteSite() {
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+
+        when(siteRepository.findById(1L)).thenReturn(Optional.of(site));
+        doNothing().when(siteRepository).delete(site);
+
+        siteService.deleteSite(1L);
+        verify(siteRepository, times(1)).delete(site);
+    }
+
+    @Test
+    public void testSaveSite() {
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+
+        when(siteRepository.save(site)).thenReturn(site);
+
+        Site savedSite = siteService.save(site);
+        assertEquals("Stade de France", savedSite.getName());
+    }
+
+    @Test
+    public void testGetSitesByName() {
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+
+        when(siteRepository.findByName("Stade de France")).thenReturn(Arrays.asList(site));
+
+        List<Site> sites = siteService.getSitesByName("Stade de France");
+        assertEquals(1, sites.size());
+        assertEquals("Stade de France", sites.get(0).getName());
+    }
+
+    @Test
+    public void testGetSportsBySiteId() {
+        Sport sport = new Sport(1L, "Basketball", new String[]{"olympic"}, null);
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, Arrays.asList(sport));
+
+        when(siteRepository.findById(1L)).thenReturn(Optional.of(site));
+
+        List<Sport> sports = siteService.getSportsBySiteId(1L);
+        assertEquals(1, sports.size());
+        assertEquals("Basketball", sports.get(0).getName());
+    }
+}
---
+++ sites-service/src/test/java/dauphine/miageif/site/service/SportServiceTest.java
@@ -0,0 +1,77 @@
+package dauphine.miageif.site.service;
+
+import dauphine.miageif.site.entity.Sport;
+import dauphine.miageif.site.repository.SportRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class SportServiceTest {
+
+    @Mock
+    private SportRepository sportRepository;
+
+    @InjectMocks
+    private SportService sportService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testGetAllSports() {
+        Sport sport1 = new Sport(1L, "Basketball", new String[]{"olympic"}, null);
+        Sport sport2 = new Sport(2L, "Swimming", new String[]{"olympic"}, null);
+
+        when(sportRepository.findAll()).thenReturn(Arrays.asList(sport1, sport2));
+
+        List<Sport> sports = sportService.getAllSports();
+        assertEquals(2, sports.size());
+        assertEquals("Basketball", sports.get(0).getName());
+        assertEquals("Swimming", sports.get(1).getName());
+    }
+
+    @Test
+    public void testSaveSport() {
+        Sport sport = new Sport(1L, "Basketball", new String[]{"olympic"}, null);
+
+        when(sportRepository.save(sport)).thenReturn(sport);
+
+        Sport savedSport = sportService.save(sport);
+        assertEquals("Basketball", savedSport.getName());
+    }
+
+    @Test
+    public void testDeleteSport() {
+        Sport sport = new Sport(1L, "Basketball", new String[]{"olympic"}, null);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(sport));
+        doNothing().when(sportRepository).delete(sport);
+
+        sportService.deleteSport(1L);
+        verify(sportRepository, times(1)).delete(sport);
+    }
+
+    @Test
+    public void testUpdateSport() {
+        Sport existingSport = new Sport(1L, "Basketball", new String[]{"olympic"}, null);
+        Sport updatedDetails = new Sport(1L, "Basketball Updated", new String[]{"olympic"}, null);
+
+        when(sportRepository.findById(1L)).thenReturn(Optional.of(existingSport));
+        when(sportRepository.save(existingSport)).thenReturn(existingSport);
+
+        Sport updatedSport = sportService.updateSport(1L, updatedDetails);
+
+        assertEquals("Basketball Updated", updatedSport.getName());
+    }
+}

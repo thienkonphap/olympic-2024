3d65c4e42604972f4eba94cd29b5fd52c43c88fa
ThienNguyen <ngocthiennguyen2102@gmail.com>
2024-05-25 21:25:26 +0200

    Add pom.xml

--- calendar-service/src/main/java/dauphine/miageif/site/repository/CalendrierSportRepository.java
+++ calendar-service/src/main/java/dauphine/miageif/site/repository/CalendrierSportRepository.java
@@ -1,7 +1,7 @@
 package dauphine.miageif.site.repository;
 
 
-import com.dauphine.miageif.site.model.CalendrierSport;
+import dauphine.miageif.site.model.CalendrierSport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

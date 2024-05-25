54fbf1db16b9d4a928bd45a9015c992b99234a54
ThienNguyen <ngocthiennguyen2102@gmail.com>
2024-05-25 21:30:31 +0200

    Add unit test for entity

---
+++ sites-service/src/test/java/dauphine/miageif/site/entity/SiteTest.java
@@ -0,0 +1,69 @@
+package dauphine.miageif.site.entity;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
+public class SiteTest {
+
+    @Test
+    public void testGettersAndSetters() {
+        // Create Sport objects
+        Sport sport1 = new Sport();
+        sport1.setSportId(1L);
+        sport1.setName("Basketball");
+        sport1.setType(new String[]{"olympic"});
+
+        Sport sport2 = new Sport();
+        sport2.setSportId(2L);
+        sport2.setName("Swimming");
+        sport2.setType(new String[]{"olympic"});
+
+        // Create Site object
+        Site site = new Site();
+        site.setSiteId(1L);
+        site.setName("Stade de France");
+        site.setLocation("Paris");
+        site.setLongitude(2.3601);
+        site.setLatitude(48.9244);
+        site.setSports(Arrays.asList(sport1, sport2));
+
+        // Test getters
+        assertEquals(1L, site.getSiteId());
+        assertEquals("Stade de France", site.getName());
+        assertEquals("Paris", site.getLocation());
+        assertEquals(2.3601, site.getLongitude());
+        assertEquals(48.9244, site.getLatitude());
+        assertEquals(2, site.getSports().size());
+        assertEquals("Basketball", site.getSports().get(0).getName());
+        assertEquals("Swimming", site.getSports().get(1).getName());
+    }
+
+    @Test
+    public void testNoArgsConstructor() {
+        // Test no-args constructor
+        Site site = new Site();
+        assertNotNull(site);
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        // Create Sport objects
+        Sport sport1 = new Sport(1L, "Basketball", new String[]{"olympic"}, null);
+        Sport sport2 = new Sport(2L, "Swimming", new String[]{"olympic"}, null);
+
+        // Create Site object using all-args constructor
+        Site site = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, Arrays.asList(sport1, sport2));
+
+        // Test getters
+        assertEquals(1L, site.getSiteId());
+        assertEquals("Stade de France", site.getName());
+        assertEquals("Paris", site.getLocation());
+        assertEquals(2.3601, site.getLongitude());
+        assertEquals(48.9244, site.getLatitude());
+        assertEquals(2, site.getSports().size());
+        assertEquals("Basketball", site.getSports().get(0).getName());
+        assertEquals("Swimming", site.getSports().get(1).getName());
+    }
+}
---
+++ sites-service/src/test/java/dauphine/miageif/site/entity/SportTest.java
@@ -0,0 +1,66 @@
+package dauphine.miageif.site.entity;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+
+public class SportTest {
+
+    @Test
+    public void testGettersAndSetters() {
+        // Create Site objects
+        Site site1 = new Site();
+        site1.setSiteId(1L);
+        site1.setName("Stade de France");
+        site1.setLocation("Paris");
+        site1.setLongitude(2.3601);
+        site1.setLatitude(48.9244);
+
+        Site site2 = new Site();
+        site2.setSiteId(2L);
+        site2.setName("Aquatics Center");
+        site2.setLocation("Paris");
+        site2.setLongitude(2.3414);
+        site2.setLatitude(48.8357);
+
+        // Create Sport object
+        Sport sport = new Sport();
+        sport.setSportId(1L);
+        sport.setName("Basketball");
+        sport.setType(new String[]{"olympic"});
+        sport.setSites(Arrays.asList(site1, site2));
+
+        // Test getters
+        assertEquals(1L, sport.getSportId());
+        assertEquals("Basketball", sport.getName());
+        assertArrayEquals(new String[]{"olympic"}, sport.getType());
+        assertEquals(2, sport.getSites().size());
+        assertEquals("Stade de France", sport.getSites().get(0).getName());
+        assertEquals("Aquatics Center", sport.getSites().get(1).getName());
+    }
+
+    @Test
+    public void testNoArgsConstructor() {
+        // Test no-args constructor
+        Sport sport = new Sport();
+        assertNotNull(sport);
+    }
+
+    @Test
+    public void testAllArgsConstructor() {
+        // Create Site objects
+        Site site1 = new Site(1L, "Stade de France", "Paris", 2.3601, 48.9244, null);
+        Site site2 = new Site(2L, "Aquatics Center", "Paris", 2.3414, 48.8357, null);
+
+        // Create Sport object using all-args constructor
+        Sport sport = new Sport(1L, "Basketball", new String[]{"olympic"}, Arrays.asList(site1, site2));
+
+        // Test getters
+        assertEquals(1L, sport.getSportId());
+        assertEquals("Basketball", sport.getName());
+        assertArrayEquals(new String[]{"olympic"}, sport.getType());
+        assertEquals(2, sport.getSites().size());
+        assertEquals("Stade de France", sport.getSites().get(0).getName());
+        assertEquals("Aquatics Center", sport.getSites().get(1).getName());
+    }
+}

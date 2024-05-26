892a4eadf1d022c4008497215a44d7deff02244d
Ouad59 <59265711+Ouad59@users.noreply.github.com>
2024-05-26 10:30:43 +0200

    delete of planning

--- calendar-service/src/main/java/dauphine/miageif/site/repository/PlanningRepository.java
+++
@@ -1,9 +0,0 @@
-package dauphine.miageif.site.repository;
-
-import dauphine.miageif.site.model.Planning;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlanningRepository extends JpaRepository<Planning, Long> {
-}

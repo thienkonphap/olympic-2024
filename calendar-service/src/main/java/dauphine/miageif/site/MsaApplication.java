83c959c65b27d918517adb22cacc572b6bf8c94d
Ouad59 <59265711+Ouad59@users.noreply.github.com>
2024-05-27 11:54:12 +0200

    calendar service vf.1

--- calendar-service/src/main/java/dauphine/miageif/site/MsaApplication.java
+++ calendar-service/src/main/java/dauphine/miageif/site/MsaApplication.java
@@ -2,8 +2,11 @@ package dauphine.miageif.site;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "dauphine.miageif.site.repository")
 public class MsaApplication {
 
 	public static void main(String[] args) {

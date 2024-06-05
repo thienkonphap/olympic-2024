415b2c686dc8bafc509cf01cd3ac275c837fa737
Ouad59 <59265711+Ouad59@users.noreply.github.com>
2024-06-05 15:14:17 +0200

    getaway

---
+++ gateway/src/main/java/dauphine/miageif/site/ApiGatewayApplication.java
@@ -0,0 +1,11 @@
+package dauphine.miageif.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+}
